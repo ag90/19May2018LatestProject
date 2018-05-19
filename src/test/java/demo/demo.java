8ce4451adb3c69b9de350cd6053988fd96a65812
Avi Gupta <guptaav@HCL.COM>
2018-05-19 16:21:09 +0530

    code changes

---
+++ src/test/java/demo/demo.java
@@ -0,0 +1,23 @@
+package demo;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class demo {
+	
+	
+
+	@Test
+	public void m()
+	{
+		assertEquals("Shri.NarendraDamodarDasModi", "PMofIndia");
+	}
+	
+	@Test
+	public void m1()
+	{
+		System.out.println("Primr Minister Office");
+	}
+
+}
